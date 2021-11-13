c256fef56b20fbd19d8b59c0477ddde022144fe4
monjurctg5 <monjurctg5@github.com>
2021-11-13 17:10:38 +0600

    hello

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId
 const app= express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
